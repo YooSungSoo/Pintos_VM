09f3df980c939827a51fe70b5c9451ab4382117e
98jh-shin <jhshin.dev@gmail.com>
2025-10-13 23:08:20 +0900

    feat(uninit, syscall): uninit 구현 및 stack_growth 대응 수정
    syscall : page_fault 라면 exit(-1) 호출 수정
    => page_fault 발생 전 stack_growth처리

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -7,7 +7,6 @@
 #include "vm/inspect.h"
 
 static struct list frame_table;  // 구조체 추가
-static bool is_valid_stack_access(void *addr, const uintptr_t rsp);
 
 /* Initializes the virtual memory subsystem by invoking each subsystem's
  * intialize codes. */
@@ -213,7 +212,7 @@ static struct frame *vm_get_frame(void) {
 }
 
 /* Growing the stack. */
-static void vm_stack_growth(void *addr UNUSED) {
+void vm_stack_growth(void *addr) {
 }
 
 /* Handle the fault on write_protected page */
@@ -391,7 +390,7 @@ bool page_less(const struct hash_elem *a, const struct hash_elem *b, void *aux U
   return page_a->va < page_b->va;
 }
 
-static bool is_valid_stack_access(void *addr, const uintptr_t rsp) {
+bool is_valid_stack_access(void *addr, const uintptr_t rsp) {
   uintptr_t fault_addr = (uintptr_t)addr;
 
   // stack 영역 확인
